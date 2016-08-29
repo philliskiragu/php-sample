d07bd7227627a9c460e8430487824ae7492acea9
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-08-30 01:52:12 +0300

    Create shapes base class and circle class extending it

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+namespace shapes;
+class Shapes
+{
+    // each shape has a name
+    protected $name;
+
+    // constructor that will require a name
+    // playing with this. more research needed
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    // set a name for a shape
+    public function setName($name){
+        return $this->name = $name;
+    }
+
+    // get a name for a shape
+    public function getName(){
+        return $this->name;
+    }
+}
+
+class Circle extends Shapes {
+    // circle has a radius as smallest unit
+    private $radius;
+
+    //constructor of the circle
+    // it initializes a shape name as "circle" and the radius
+    public function __construct($radius){
+        // parent constructor call
+        parent::__construct("circle");
+        $this->radius = $radius;
+    }
+
+
+    // set radius
+    function setRadius($radius) {
+        $this->radius = $radius;
+    }
+
+    // get radius
+    function getRadius() {
+        return $this->radius;
+    }
+
+    // PI is a constant value in circle calculations
+    const PI = 3.142;
+
+    // get circle area ( pi * r^2)
+    public function getCircleArea($radius){
+        $this->radius = $radius;
+        return pow($this->radius, 2) * self::PI;
+    }
+
+    // get circle perimeter (2 * pi * r)
+    public function getCirclePerimeter($radius){
+        $this->radius = $radius;
+        return 2 * $this->radius * self::PI;
+    }
+
+    // get the circle surface area (2 * pi * r^2)
+    public function getCircleSurfaceArea($radius){
+        $this->radius = $radius;
+        return 2 * pow($this->radius, 2) * self::PI;
+    }
+}
+
+$shapeObject = new Circle(7);
+echo $shapeObject->getCircleArea($shapeObject->getRadius());
+?>
